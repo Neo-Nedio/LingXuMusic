2f09ac356e279c8562daebeaf019e04ac70b3591
hasya <Nedio7@qq.com>
2026-05-23 18:22:30 +0800

    feat:Theme 3

---
+++ app/src/main/java/com/neo/lingxumusic/utils/ComposeExt.kt
@@ -0,0 +1,44 @@
+package com.neo.lingxumusic.utils
+
+import android.content.res.Resources
+import android.util.TypedValue
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.sp
+
+// 缓存屏幕信息，避免重复获取
+private val screenWidthDp by lazy {
+    //像素/密度 = dp
+    Resources.getSystem().displayMetrics.widthPixels / Resources.getSystem().displayMetrics.density
+}
+
+//设计稿宽度
+private const val designWidth = 750f
+
+/**
+ * compose屏幕适配单位
+ */
+val Number.cdp
+    get() = Dp(
+        toFloat() * //toFloat是Number本身，有上下文
+                screenWidthDp / designWidth
+    )
+
+val Dp.toPx
+    get() = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, value, Resources.getSystem().displayMetrics)
+
+/**
+ * compose屏幕适配单位（字体专用）
+ */
+val Number.csp
+    get() = (toFloat() *
+            Resources.getSystem().displayMetrics.widthPixels
+            / 750
+            / Resources.getSystem().displayMetrics.scaledDensity).sp
+
+
+/**
+ * 将数字转换成compose中的DP
+ */
+val Number.transformDp
+    get() = Dp(toFloat() / Resources.getSystem().displayMetrics.density)
+
